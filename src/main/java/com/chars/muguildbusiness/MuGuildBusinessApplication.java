fb1a012aab62bd04bce7caeacdb0cfdc0a27fa5d
Carlos Becerra <carlosbecerra.dev@gmail.com>
2020-09-23 18:41:06 -0500

    initial desktop commit

---
+++ src/main/java/com/chars/muguildbusiness/MuGuildBusinessApplication.java
@@ -0,0 +1,13 @@
+package com.chars.muguildbusiness;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MuGuildBusinessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MuGuildBusinessApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/chars/muguildbusiness/MuGuildBusinessApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chars.muguildbusiness;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MuGuildBusinessApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
