d9b2ae393c16ae5b782d52bed2567795b25766d5
Carlos Becerra <carlosbecerra.dev@gmail.com>
2020-09-25 17:17:30 -0500

    :pencil: Olvidé el aguegar el @PostMapping

--- src/main/java/com/chars/muguildbusiness/controller/AuthController.java
+++ src/main/java/com/chars/muguildbusiness/controller/AuthController.java
@@ -3,6 +3,7 @@ package com.chars.muguildbusiness.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,6 +18,7 @@ public class AuthController {
 	@Autowired
 	private AuthService authService;
 	
+	@PostMapping("/signup")
 	public ResponseEntity<String> signup(@RequestBody RegisterRequest registerRequest){
 		authService.signup(registerRequest);
 		return new ResponseEntity<>("User Registration Successful", HttpStatus.OK);
