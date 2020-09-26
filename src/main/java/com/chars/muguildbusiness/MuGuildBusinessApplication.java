d24aaa1ed5c0506cd1b1df4a20e2faa3655622c9
Carlos Becerra <carlosbecerra.dev@gmail.com>
2020-09-26 17:34:24 -0500

    :green_heart: sendMail: Agregando asincronía al enviar el mail

--- src/main/java/com/chars/muguildbusiness/MuGuildBusinessApplication.java
+++ src/main/java/com/chars/muguildbusiness/MuGuildBusinessApplication.java
@@ -2,9 +2,11 @@ package com.chars.muguildbusiness;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 
 @SpringBootApplication
+@EnableAsync
 public class MuGuildBusinessApplication {
 	
 	public static void main(String[] args) {
