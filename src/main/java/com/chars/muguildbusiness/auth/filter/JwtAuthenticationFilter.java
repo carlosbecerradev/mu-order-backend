df26f7ff75b0758c40bd9b4f6187fd0de8119795
Carlos Becerra <carlosbecerra.dev@gmail.com>
2020-09-28 17:44:10 -0500

    :heavy_plus_sign: login: Implementando el método attemptAuthentication() para obtener el username y password con los cuales se buscará el usuario en la base de datos.

---
+++ src/main/java/com/chars/muguildbusiness/auth/filter/JwtAuthenticationFilter.java
@@ -0,0 +1,67 @@
+package com.chars.muguildbusiness.auth.filter;
+
+import java.io.IOException;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+import com.chars.muguildbusiness.dto.LoginRequest;
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class JwtAuthenticationFilter extends
+	UsernamePasswordAuthenticationFilter {
+	
+	private AuthenticationManager authenticationManager;
+
+	public JwtAuthenticationFilter(AuthenticationManager authenticationManager) {
+		this.authenticationManager = authenticationManager;
+		setRequiresAuthenticationRequestMatcher(new AntPathRequestMatcher("/api/login", "POST"));
+	}
+
+	@Override
+	public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response)
+			throws AuthenticationException {
+
+		String username = "", password = "";
+		
+		try {
+			LoginRequest loginRequest = 
+					new ObjectMapper().readValue(request.getInputStream(), LoginRequest.class);
+			
+			username = loginRequest.getUsername();
+			password = loginRequest.getPassword();
+			
+			logger.info("username raw(json): - " + username);
+			logger.info("password raw(json): - " + password);
+			
+		} catch (JsonParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (JsonMappingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		username = username.trim();
+
+		UsernamePasswordAuthenticationToken authRequest = new UsernamePasswordAuthenticationToken(
+				username, password);
+		
+		return authenticationManager.authenticate(authRequest);
+	}
+
+	
+	
+}
--- src/main/java/com/chars/muguildbusiness/config/SpringSecurityConfig.java
+++ src/main/java/com/chars/muguildbusiness/config/SpringSecurityConfig.java
@@ -5,8 +5,11 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+
+import com.chars.muguildbusiness.auth.filter.JwtAuthenticationFilter;
 
 @EnableWebSecurity
 @Configuration
@@ -15,11 +18,15 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 
-		http.csrf().disable()
-			.authorizeRequests()
+		http.authorizeRequests()
 			.antMatchers("/api/auth/**").permitAll()
 			.antMatchers("/api/customer").permitAll()
-			.anyRequest().authenticated();
+			.anyRequest().authenticated()
+			.and()
+			.addFilter(new JwtAuthenticationFilter(authenticationManager()))
+			.csrf().disable()
+			.sessionManagement()
+				.sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 	}
 	
 	@Bean
