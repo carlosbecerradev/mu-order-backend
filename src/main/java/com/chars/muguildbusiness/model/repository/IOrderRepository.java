b7ed77dc2adc0b58748498330e3bd0f6e5ec5ff9
Carlos Becerra <carlosbecerra.dev@gmail.com>
2020-10-09 16:09:40 -0500

    :heavy_plus_sign: api order: Se creo el método get para obtener todas las ordenes.

--- src/main/java/com/chars/muguildbusiness/model/repository/IOrderRepository.java
+++ src/main/java/com/chars/muguildbusiness/model/repository/IOrderRepository.java
@@ -1,11 +1,12 @@
 package com.chars.muguildbusiness.model.repository;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import com.chars.muguildbusiness.model.entity.Order;
 
 @Repository
-public interface IOrderRepository extends CrudRepository<Order, Long> {
+public interface IOrderRepository extends JpaRepository<Order, Long> {
 
 }
---
+++ src/main/java/com/chars/muguildbusiness/model/service/OrderServiceImpl.java
@@ -0,0 +1,68 @@
+package com.chars.muguildbusiness.model.service;
+
+import java.util.List;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.chars.muguildbusiness.dto.OrderRequest;
+import com.chars.muguildbusiness.dto.OrderResponse;
+import com.chars.muguildbusiness.model.entity.Order;
+import com.chars.muguildbusiness.model.repository.IOrderRepository;
+
+@Service
+public class OrderServiceImpl implements OrderService {
+	
+	@Autowired
+	private IOrderRepository orderRepository;
+
+	@Override
+	public void save(OrderRequest order) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	@Transactional(readOnly = true)
+	public List<OrderResponse> findAll() {	
+		return orderRepository.findAll()
+				.stream().map(this::mapToDto)
+				.collect(Collectors.toList());		
+	}
+	
+	private OrderResponse mapToDto(Order order) {
+		OrderResponse orderResponse = new OrderResponse();
+		orderResponse.setId(order.getOrder_id());
+		orderResponse.setItemName(order.getItem().getName());
+		orderResponse.setCreatedAt(order.getCreated());
+		orderResponse.setItemLevel(order.getItem_level());
+		orderResponse.setItemOption(order.getItem_options());
+		orderResponse.setItemType(order.getItem_type());
+		orderResponse.setNickname(order.getUser().getNickname());
+		orderResponse.setObservation(order.getObservation());
+		
+		return orderResponse;
+	}
+
+	@Override
+	public List<OrderResponse> findAllByItemName(String itemName) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<OrderResponse> findAllByItemCategoryName(String itemCategoryName) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void delete(Long id) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
