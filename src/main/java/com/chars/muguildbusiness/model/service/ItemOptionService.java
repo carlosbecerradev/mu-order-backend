59e921792c7bfcce9cdd7d335e627f7b16e48801
Carlos Becerra <carlosbecerra.dev@gmail.com>
2020-10-14 15:54:42 -0500

    :x: app: Se removio clases no usadas.

--- src/main/java/com/chars/muguildbusiness/model/service/ItemOptionService.java
+++
@@ -1,9 +0,0 @@
-package com.chars.muguildbusiness.model.service;
-
-import java.util.List;
-
-import com.chars.muguildbusiness.dto.ItemOptionResponse;
-
-public interface ItemOptionService {
-	public List<ItemOptionResponse> findAllByItemId(Long itemId);
-}
--- src/main/java/com/chars/muguildbusiness/model/service/ItemTypeService.java
+++
@@ -1,9 +0,0 @@
-package com.chars.muguildbusiness.model.service;
-
-import java.util.List;
-
-import com.chars.muguildbusiness.dto.ItemTypeResponse;
-
-public interface ItemTypeService {
-	public List<ItemTypeResponse> findAllByItemId(Long itemId);
-}
